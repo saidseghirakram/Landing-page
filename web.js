13602c6e15d55323e4c8288d8e45bd66e58980de
SAID SEGHIR AKRAM <116347576+saidseghirakram@users.noreply.github.com>
2022-11-17 04:46:03 -0800

    Add files via upload

---
+++ web.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.img');
+let container=document.querySelector('.container ');
+function color(phone){
+img.src=phone 
+}
+function bg(color){
+    container.style.background = color;
+}
